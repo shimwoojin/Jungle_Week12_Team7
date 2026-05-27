332b1dbf972f2cf802164ca79b4912303ecfd0a7
kael <sskimhd34@gmail.com>
2026-05-28 02:18:18 +0900

    Docs: README.md 추가
    Changes to be committed:
    	modified:   KraftonEngine/Source/Engine/Platform/BuildInfo.h
    	new file:   README.md

--- KraftonEngine/Source/Engine/Platform/BuildInfo.h
+++ KraftonEngine/Source/Engine/Platform/BuildInfo.h
@@ -4,8 +4,8 @@ namespace BuildInfo
 {
 	inline constexpr const char* ProductName = "KraftonEngine_Team7";
 	inline constexpr const char* BuildConfig = "Release";
-	inline constexpr const char* BuildVersion = "20260527_2146";
-	inline constexpr const char* GitCommit = "b8565c29";
+	inline constexpr const char* BuildVersion = "20260528_0140";
+	inline constexpr const char* GitCommit = "6f6a2c73";
 	inline constexpr const char* SymbolPath = "srv*C:\\SymbolCache*\\\\SYMBOL-SERVER\\Symbols\\Team7";
-	inline constexpr const char* BuildTime = "2026-05-27 21:46:45";
+	inline constexpr const char* BuildTime = "2026-05-28 01:40:15";
 }
