cb63f5decf9155ea5de30d43d76b825e68dc56a9
kael <sskimhd34@gmail.com>
2026-05-27 22:08:15 +0900

    Docs: Release 빌드 경로를 PackageRelease.bat으로 단일화
    ReleaseBuild.bat 삭제
    심볼 서버에 업로드된 PDB와 배포 EXE가 어긋나는 문제를 방지
    Release 배포는 PackageRelease.bat [VersionName]을 통해서만 수행하도록 정리
    PackageRelease.bat에서 BuildInfo 생성, Release 빌드, 패키지 구성, source indexing, 심볼 업로드까지 한 번에 처리
    일반 Release 빌드와 심볼 업로드된 Release 빌드가 분리되는 혼동 제거
    
    Changes to be committed:
    	modified:   KraftonEngine/Source/Engine/Platform/BuildInfo.h
    	deleted:    ReleaseBuild.bat

--- KraftonEngine/Source/Engine/Platform/BuildInfo.h
+++ KraftonEngine/Source/Engine/Platform/BuildInfo.h
@@ -4,8 +4,8 @@ namespace BuildInfo
 {
 	inline constexpr const char* ProductName = "KraftonEngine_Team7";
 	inline constexpr const char* BuildConfig = "Release";
-	inline constexpr const char* BuildVersion = "20260527_2100";
-	inline constexpr const char* GitCommit = "62f45713";
+	inline constexpr const char* BuildVersion = "20260527_2146";
+	inline constexpr const char* GitCommit = "b8565c29";
 	inline constexpr const char* SymbolPath = "srv*C:\\SymbolCache*\\\\SYMBOL-SERVER\\Symbols\\Team7";
-	inline constexpr const char* BuildTime = "2026-05-27 21:00:04";
+	inline constexpr const char* BuildTime = "2026-05-27 21:46:45";
 }
